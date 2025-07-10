c51675fffaca938927017823cc7c090bdc19856a
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-07-10 11:59:32 +0300

    refactor build scripts

--- build.js
+++ build.js
@@ -1,45 +1,81 @@
 import fs from 'fs';
-
-export function init() {
-    if(!fs.existsSync('./data')) {
-        fs.mkdirSync('./data');
-    }
-    get_data();
-}
-
-function determine_route_type(type) {
-    const bindings = {
-        'autobus': 'bus',
-        'trolleybus': 'trolley',
-        'tramway': 'tram',
-        'metro': 'metro'
-    };
-    if(bindings[type]) {
-        return bindings[type];
-    }
-    if(['bus', 'trolley', 'tram', 'metro'].includes(type)) {
 import { fetch_file, get_date, get_latest_data_commit } from './utils.js';
+
+const MAIN_TYPES = ['bus', 'trolley', 'tram', 'metro'];
+
+const route_overrides = JSON.parse(fs.readFileSync('./route_overrides.json', 'utf-8') || '{}');
+
+function normalise_route(route, date) {
+    function determine_route_type(type) {
+        const bindings = {
+            'autobus': 'bus',
+            'trolleybus': 'trolley',
+            'tramway': 'tram',
+            'metro': 'metro'
+        };
+        if(bindings[type]) {
+            return bindings[type];
+        }
+        if(MAIN_TYPES.includes(type)) {
+            return type;
+        }
+        console.error('Unknown route type:', type);
         return type;
     }
-    console.error('Unknown route type:', type);
-    return type;
+    function get_route_ref(route) {
+        let route_ref = (route.route_ref?route.route_ref:route.line).toString().trim().replaceAll(' ', '');
+        if(route_ref.startsWith('Y')) {
+            route_ref = 'У'+route_ref.slice(1);
+        }
+        //                          Cyrl                         Latn
+        else if(route_ref.endsWith('ТМ') || route_ref.endsWith('TM')) {
+            route_ref = route_ref.slice(0, -2) + 'ТМ'; // Cyrl
+        }
+        //                         Cyrl                         Latn
+        else if(route_ref.endsWith('А') || route_ref.endsWith('A')) {
+            route_ref = route_ref.slice(0, -1) + 'А'; // Cyrl
+        }
+        //                          Cyrl                         Latn
+        else if(route_ref.startsWith('Х') || route_ref.startsWith('X')) {
+            route_ref = 'X'+route_ref.slice(1); // Latn
+        }
+        if(route_ref.includes('-')) {
+            route_ref = route_ref.replace('-', '');
+        }
+        return route_ref;
+    }
+    function get_route_subtype(route) {
+        if(route.type !== 'bus') {
+            return null;
+        }
+        if(route.route_ref.startsWith('У')) {
+            return 'school';
+        }
+        else if(route.route_ref.startsWith('N')) {
+            return 'night';
+        }
+        else if(route.route_ref.endsWith('ТМ')) {
+            return 'temporary';
+        }
+        return null;
+    }
+    route.type = determine_route_type(route.type);
+    route.route_ref = get_route_ref(route);
+    for(const override of route_overrides) {
+        if(override.old.type != route.type || override.old.route_ref != route.route_ref) {
+            continue;
+        }
+        if(date < override.start_date || date > override.end_date) {
+            continue;
+        }
+        route.type = override.new.type;
+        route.route_ref = override.new.route_ref;
+        break;
+    }
+    route.subtype = get_route_subtype(route);
 }
 
-function get_route_ref(route) {
-    let route_ref = (route.route_ref?route.route_ref:route.line).toString();
-    if(route_ref.startsWith('Y')) {
-        route_ref = 'У'+route_ref.slice(1);
-    }
-    //                          Cyrl                         Latn
-    else if(route_ref.endsWith('-ТМ') || route_ref.endsWith('-TM')) {
-        route_ref = route_ref.slice(0, -3) + 'ТМ'; //Cyrl
-    }
-    //                         Cyrl                         Latn
-    else if(route_ref.endsWith('-А') || route_ref.endsWith('-A')) {
-        route_ref = route_ref.slice(0, -2) + 'А'; //Cyrl
-    }
-    return route_ref;
-}
+
 
 function is_weekend(valid_thru) {
     if(valid_thru === '100') {
@@ -92,7 +128,7 @@ function do_schedules_overlap(old_schedule, new_schedule) {
     return false;
 }
 
-export async function get_data(commit, current_date, yesterday_date) {
+export async function get_data(routes_data, processed_routes, commit, current_date, yesterday_date) {
     if(!commit) {
         commit = await get_latest_data_commit();
     }
@@ -111,29 +147,23 @@ export async function get_data(commit, current_date, yesterday_date) {
     const remote_routes = await fetch_file('routes.json', commit);
     const trips = await fetch_file('trips.json', commit);
 
-    remote_routes.forEach((route, index) => {
-        route.route_index = index;
-    });
+    remote_routes.forEach((route, route_index) => {
+        normalise_route(route, current_date);
+        
+        const type = route.type;
+        const route_ref = route.route_ref;
+        const subtype = route.subtype || null;
 
-    for(const route of remote_routes) {
-        const type = determine_route_type(route.type);
-        const route_ref = get_route_ref(route);
-
-        const filename = `./data/${type}_${route_ref}.json`;
-        let data;
-        if(!fs.existsSync(filename)) {
-            data = {
-                type: type,
-                route_ref: route_ref,
-                schedules: []
-            }
-        }
-        else {
-            data = JSON.parse(fs.readFileSync(filename));
-        }
+        const data = routes_data.get(`${type}/${route_ref}`) || {
+            type: type,
+            route_ref: route_ref,
+            subtype: subtype,
+            schedules: []
+        };
+        processed_routes.add(`${type}/${route_ref}`);
 
         trips
-        .filter(trip => trip.route_index === route.route_index)
+        .filter(trip => trip.route_index === route_index)
         .forEach(trip => {
             if(trip.valid_from) {
                 trip.valid_from = trip.valid_from.split('.').toReversed().join('-');
@@ -158,8 +188,9 @@ export async function get_data(commit, current_date, yesterday_date) {
                 return;
             }
             const similar_schedules = data.schedules.filter(schedule => !schedule.end_date && do_schedules_overlap(schedule.is_weekend, trip.is_weekend));
+            const not_metro_m1_or_m2 = (type !== 'metro' && route_ref !== 'M1' && route_ref !== 'M2');
             for(const schedule of similar_schedules) {
-                if(!schedule.end_date) {
+                if(!schedule.end_date && not_metro_m1_or_m2) {
                     schedule.end_date = yesterday_date;
                 }
             }
@@ -170,32 +201,8 @@ export async function get_data(commit, current_date, yesterday_date) {
             });
         });
 
-        console.log(`Writing to ${filename} for date ${current_date}`);
-        fs.writeFileSync(filename, JSON.stringify(data));
-    }
-
-    const local_routes = fs.readdirSync('./data');
-    const remote_route_tags = new Set();
-    remote_routes.forEach(route => {
-        remote_route_tags.add(`${determine_route_type(route.type)}_${get_route_ref(route)}`);
-    });
-    
-    const local_route_tags = new Set();
-    local_routes.forEach(route => {
-        local_route_tags.add(route.split('.')[0]);
-    });
-
-    const to_remove = local_route_tags.difference(remote_route_tags);
-    to_remove.forEach(tag => {
-        console.log(`Removing ${tag}`);
-        const data  = JSON.parse(fs.readFileSync(`./data/${tag}.json`));
-        data.schedules.forEach(schedule => {
-            if(!schedule.end_date) {
-                schedule.end_date = yesterday_date;
-            }
-        });
-        fs.writeFileSync(`./data/${tag}.json`, JSON.stringify(data));
+        if(!routes_data.has(`${type}/${route_ref}`)) {
+            routes_data.set(`${type}/${route_ref}`, data);
+        }
     });
 }
-
-// init();
--- build_data.js
+++ build_data.js
@@ -1,21 +1,76 @@
 import fs from 'fs';
 import { get_data } from './build.js';
+import { start } from 'repl';
 
-if(fs.existsSync('./data')) {
-    fs.rmdirSync('./data', { recursive: true });
+if(!fs.existsSync('./data')) {
+    fs.mkdirSync('./data');
+//     fs.rmdirSync('./data', { recursive: true });
 }
-fs.mkdirSync('./data');
 
-const ordered_commits = final_commits.toReversed();
-let limit = 0;
-for(const [date, commit] of ordered_commits) {
-    const today = date;
-    const yesterday = new Date(today);
+for(const type of ['bus', 'trolley', 'tram', 'metro']) {
+    if(!fs.existsSync(`./data/${type}`)) {
+        fs.mkdirSync(`./data/${type}`);
+    }
+}
+
+const commits = JSON.parse(fs.readFileSync('commits.json')).toReversed();
+try {
+    var start_date = fs.readFileSync('last_run.txt', 'utf-8');
+}
+catch(e) {
+    start_date = '2023-10-01';
+}
+
+let limit = 500;
+let yesterday, today;
+
+const routes_data = new Map();
+for(const type of ['bus', 'trolley', 'tram', 'metro']) {
+    const files = fs.readdirSync(`./data/${type}`);
+    for(const file of files) {
+        const route_ref = file.replace('.json', '');
+        const data = JSON.parse(fs.readFileSync(`./data/${type}/${file}`, 'utf-8'));
+        routes_data.set(`${type}/${route_ref}`, data);
+    }
+}
+
+for(const [commit_date, commit] of commits) {
+    if(commit_date <= start_date) {
+        continue;
+    }
+    
+    yesterday = new Date(commit_date);
+    today = new Date(commit_date);
     yesterday.setDate(yesterday.getDate() - 1);
     console.log(today);
-    await get_data(commit, today, yesterday.toISOString().split('T')[0]);
+    const processed_routes = new Set();
+    await get_data(routes_data, processed_routes, commit, today.toISOString().split('T')[0], yesterday.toISOString().split('T')[0]);
+
+    for(const route_tag of routes_data.keys()) {
+        if(!processed_routes.has(route_tag)) {
+            const data = routes_data.get(route_tag);
+            // This route was not processed, so we mark its open schedules as ended
+            data.schedules.forEach(schedule => {
+                if(!schedule.end_date) {
+                    schedule.end_date = yesterday.toISOString().split('T')[0];
+                }
+            });
+
+        }
+    }
+
     limit--;
     if(limit === 0) {
         break;
     }
 }
+fs.writeFileSync('last_run.txt', today.toISOString().split('T')[0], 'utf-8');
+
+routes_data.entries().forEach(([route_tag, data]) => {
+    const [type, route_ref] = route_tag.split('/');
+    const filename = `./data/${type}/${route_ref}.json`;
+    if(!fs.existsSync(`./data/${type}`)) {
+        fs.mkdirSync(`./data/${type}`);
+    }
+    fs.writeFileSync(filename, JSON.stringify(data), 'utf-8');
+});
