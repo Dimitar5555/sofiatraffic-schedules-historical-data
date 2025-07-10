9dee93e0969a91c6ae2cb70f757669d93bfcd447
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-07-10 11:56:44 +0300

    refactor build docs script

--- build_docs.js
+++ build_docs.js
@@ -1,22 +1,49 @@
 import fs from 'fs';
 
+if(fs.existsSync('./dist/data')) {
+    fs.rmSync('./dist/data', { recursive: true });
+}
+
+if(!fs.existsSync('./dist/data')) {
+    fs.mkdirSync('./dist/data');
+}
+
 const commits = JSON.parse(fs.readFileSync('commits.json'));
 
-const routes_files = fs.readdirSync('./data')
-.map(file => {
-    const data = file.split('.')[0].split('_');
-    const type = data[0];
-    const route_ref = data[1];
-    return {type, route_ref};
+const sorting_by_type = ['metro', 'tram', 'trolley', 'bus'];
+
+const routes_files = sorting_by_type.flatMap(type => {
+    if(!fs.existsSync(`./data/${type}`)) {
+        return;
+    }
+    const files = fs.readdirSync(`./data/${type}`);
+    const list = files.map(file => {
+        const data = JSON.parse(fs.readFileSync(`./data/${type}/${file}`, 'utf-8'));
+        const is_active = data.schedules.some(schedule => !schedule.end_date);
+        if(!fs.existsSync(`./dist/data/${type}/`)) {
+            fs.mkdirSync(`./dist/data/${type}/`, { recursive: true });
+        }
+        fs.writeFileSync(`./dist/data/${type}/${file}`, JSON.stringify(data));
+        return {
+            type: data.type,
+            subtype: data.subtype || undefined,
+            route_ref: data.route_ref,
+            is_active
+        };
+    })
+    .toSorted((a, b) => {
+        if(sorting_by_type.indexOf(a.type) < sorting_by_type.indexOf(b.type)) {
+            return -1;
+        }
+        if(sorting_by_type.indexOf(a.type) > sorting_by_type.indexOf(b.type)) {
+            return 1;
+        }
+        const numeric_a = parseInt(a.route_ref.replaceAll(/[^0-9]/g, ''));
+        const numeric_b = parseInt(b.route_ref.replaceAll(/[^0-9]/g, ''));
+        return numeric_a - numeric_b;
+    });
+    return list;
 });
 
-if(!fs.existsSync('./docs/data')) {
-    fs.mkdirSync('./docs/data');
-}
-
-fs.writeFileSync('./docs/data/commits.json', JSON.stringify(commits));
-fs.writeFileSync('./docs/data/routes.json', JSON.stringify(routes_files));
-for(const [route_type, route_ref] of routes_files) {
-    const route_data = fs.readFileSync(`./data/${route_type}_${route_ref}.json`);
-    fs.writeFileSync(`./docs/data/${route_type}_${route_ref}.json`, route_data);
-}
+fs.writeFileSync('./dist/data/commits.json', JSON.stringify(commits));
+fs.writeFileSync('./dist/data/routes.json', JSON.stringify(routes_files));
