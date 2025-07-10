f1d4fcfd2de55a8e5957373a5c8aea71ead76cdb
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-07-10 11:51:32 +0300

    update method for commits list

--- build_commits_list.js
+++ build_commits_list.js
@@ -5,4 +5,4 @@ import { build_commit_history } from './utils.js';
 const old_commits = JSON.parse(fs.readFileSync('commits.json'));
 const new_commits = await build_commit_history(0, old_commits[0][1]);
 const final_commits = new_commits.concat(old_commits);
-fs.writeFileSync('commits.json', JSON.stringify(final_commits));
+fs.writeFileSync('commits.json', JSON.stringify(final_commits, null, 2), 'utf-8');
