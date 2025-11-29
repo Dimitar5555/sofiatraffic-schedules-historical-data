83309a2ec2f3580ecd80a87fdb230e979c869d2f
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-11-30 00:28:39 +0200

    update build scripts

--- scripts/build_data.js
+++ scripts/build_data.js
@@ -1,35 +1,34 @@
 import fs from 'fs';
 import { get_data } from './build.js';
-import { start } from 'repl';
 
-if(!fs.existsSync('/data')) {
-    fs.mkdirSync('/data');
+if(!fs.existsSync('data')) {
+    fs.mkdirSync('data');
     // fs.rmdirSync('/data', { recursive: true });
 }
 
 for(const type of ['bus', 'trolley', 'tram', 'metro']) {
-    if(!fs.existsSync(`/data/${type}`)) {
-        fs.mkdirSync(`/data/${type}`);
+    if(!fs.existsSync(`data/${type}`)) {
+        fs.mkdirSync(`data/${type}`);
     }
 }
 
-const commits = JSON.parse(fs.readFileSync('/commits.json')).toReversed();
+const commits = JSON.parse(fs.readFileSync('commits.json')).toReversed();
 try {
-    var start_date = fs.readFileSync('/data/last_run.txt', 'utf-8');
+    var start_date = fs.readFileSync('data/last_run.txt', 'utf-8');
 }
 catch(e) {
     start_date = '2023-10-01';
 }
 
-let limit = 500;
+let limit = 0;
 let yesterday, today;
 
 const routes_data = new Map();
 for(const type of ['bus', 'trolley', 'tram', 'metro']) {
-    const files = fs.readdirSync(`/data/${type}`);
+    const files = fs.readdirSync(`data/${type}`);
     for(const file of files) {
         const route_ref = file.replace('.json', '');
-        const data = JSON.parse(fs.readFileSync(`/data/${type}/${file}`, 'utf-8'));
+        const data = JSON.parse(fs.readFileSync(`data/${type}/${file}`, 'utf-8'));
         routes_data.set(`${type}/${route_ref}`, data);
     }
 }
@@ -61,16 +60,16 @@ for(const [commit_date, commit] of commits) {
 
     limit--;
     if(limit === 0) {
+        fs.writeFileSync('data/last_run.txt', commit_date, 'utf-8');
         break;
     }
 }
-fs.writeFileSync('/data/last_run.txt', new Date().toISOString().split('T')[0], 'utf-8');
 
 routes_data.entries().forEach(([route_tag, data]) => {
     const [type, route_ref] = route_tag.split('/');
-    const filename = `/data/${type}/${route_ref}.json`;
-    if(!fs.existsSync(`/data/${type}`)) {
-        fs.mkdirSync(`/data/${type}`);
+    const filename = `data/${type}/${route_ref}.json`;
+    if(!fs.existsSync(`data/${type}`)) {
+        fs.mkdirSync(`data/${type}`);
     }
     fs.writeFileSync(filename, JSON.stringify(data, null, 2), 'utf-8');
 });
--- scripts/build_dist.js
+++ scripts/build_dist.js
@@ -1,16 +1,16 @@
 import fs from 'fs';
 
-const commits = JSON.parse(fs.readFileSync('/commits.json'));
+const commits = JSON.parse(fs.readFileSync('commits.json'));
 
 const sorting_by_type = ['metro', 'tram', 'trolley', 'bus'];
 
 const routes_files = sorting_by_type.flatMap(type => {
-    if(!fs.existsSync(`/data/${type}`)) {
+    if(!fs.existsSync(`data/${type}`)) {
         return;
     }
-    const files = fs.readdirSync(`/data/${type}`);
+    const files = fs.readdirSync(`data/${type}`);
     const list = files.map(file => {
-        const data = JSON.parse(fs.readFileSync(`/data/${type}/${file}`, 'utf-8'));
+        const data = JSON.parse(fs.readFileSync(`data/${type}/${file}`, 'utf-8'));
         const is_active = data.schedules.some(schedule => !schedule.end_date);
         return {
             type: data.type,
@@ -33,5 +33,5 @@ const routes_files = sorting_by_type.flatMap(type => {
     return list;
 });
 
-fs.writeFileSync('/data/commits.json', JSON.stringify(commits));
-fs.writeFileSync('/data/routes.json', JSON.stringify(routes_files));
+fs.writeFileSync('data/commits.json', JSON.stringify(commits));
+fs.writeFileSync('data/routes.json', JSON.stringify(routes_files));
