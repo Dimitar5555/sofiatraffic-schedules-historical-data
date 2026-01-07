960f0e2982133a3737c53d82b850e4cee562e543
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-01-07 14:52:41 +0200

    Update build_commits_list.js

--- scripts/build_commits_list.js
+++ scripts/build_commits_list.js
@@ -2,7 +2,17 @@ import fs from 'fs';
 import { build_commit_history } from './utils.js';
 
 const commits_file = 'data/commits.json';
+if(!fs.existsSync(commits_file)) {
+    fs.writeFileSync(commits_file, '[]', 'utf-8');
+}
 const old_commits = JSON.parse(fs.readFileSync(commits_file));
-const new_commits = await build_commit_history(0, old_commits[0][1]);
-const final_commits = new_commits.concat(old_commits);
-fs.writeFileSync(commits_file, JSON.stringify(final_commits, null, 2), 'utf-8');
+if(old_commits.length === 0) {
+    console.log('No existing commits, building full history...');
+    const all_commits = await build_commit_history();
+    fs.writeFileSync(commits_file, JSON.stringify(all_commits, null, 2), 'utf-8');
+}
+else {
+    const new_commits = await build_commit_history(0, old_commits[0][1]);
+    const final_commits = new_commits.concat(old_commits);
+    fs.writeFileSync(commits_file, JSON.stringify(final_commits, null, 2), 'utf-8');
+}
